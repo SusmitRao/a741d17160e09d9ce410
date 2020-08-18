b6d40d9f5ae86e09c788410599c0b215dbbff472
Susmit Rao <susmit@nurturelabs.co>
2020-08-18 17:31:24 +0530

    small cahge

--- my-app/src/Asteroid.js
+++ my-app/src/Asteroid.js
@@ -15,7 +15,11 @@ class Asteroid extends React.Component {
       <div className="container">
         <p> Name -{asteroid && asteroid.name}</p>
         <p> Nasa_JPL_URL -{asteroid && asteroid.nasa_jpl_url}</p>
-        <p>{asteroid && `${asteroid.is_potentially_hazardous_asteroid}`}</p>
+        <p>
+          {" "}
+          Is potentially hazardous-
+          {asteroid && `${asteroid.is_potentially_hazardous_asteroid}`}
+        </p>
       </div>
     );
   }
